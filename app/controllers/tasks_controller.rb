f61d880a0c5c57e9f47e139c0fef05138dd95615
minhthuan274 <doanminhthuan274@gmail.com>
2017-06-16 16:37:08 +0700

    change done and todo count make faster, maybe

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
     @list = List.find(params[:list_id])
     @task = @list.tasks.build(title: params[:title], complete: false)
     if @task.save 
-      @list.add_todo
+      # @list.add_todo
       render json: @task, status: :created, location: @task
     else
       render json: @task.errors
@@ -23,13 +23,13 @@ class TasksController < ApplicationController
 
   def update
     @list = List.find_by_id(@task.list_id).add_done
-    @task.set_done
+    # @task.set_done
     head :ok
   end
 
   def destroy 
     @list = List.find_by_id(@task.list_id).remove_done
-    @task.destroy 
+    # @task.destroy 
     head :ok
   end
 
--- app/views/lists/manage.json.jbuilder
+++ app/views/lists/manage.json.jbuilder
@@ -1,4 +1,14 @@
 json.lists @lists do |list|
-  json.extract! list, :id, :title, :todo_count, :done_count
+  todo_count = done_count = 0
+  list.tasks.each do |task| 
+    if task.complete 
+      done_count += 1
+    else
+      todo_count += 1
+    end
+  end
+  json.extract! list, :id, :title
+  json.todo_count todo_count
+  json.done_count done_count
   json.user_email User.find_by_id(list.user_id).email
 end
